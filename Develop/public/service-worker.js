31dc879f829a3cf419cdac2c2a3f944d5e1cdb1e
Dominador G de la Cruz Jr <domenikos858@gmail.com>
2020-07-20 17:02:55 -0700

    ok

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,9 +1,14 @@
 var CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE = 'my-data-cache-v1';
 
 var urlsToCache = [
   '/',
   '/styles.css',
-  '/index.js'
+  '/index.js',
+  '/db.js',
+  'manifest.json',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
 ];
 
 
@@ -19,6 +24,19 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
+  if(event.request.url.includes("api")){
+    event.respondWith(
+      caches.open(DATA_CACHE).then(cache => {
+        return fetch(event.request)
+        .then(response => {
+          cache.put(event.request, response.clone())
+          return response;
+        })
+        .catch(()=> caches.match(event.request))
+      })
+    )
+    return
+  }
   event.respondWith(
     fetch(event.request).catch(function() {
       return caches.match(event.request).then(function(response) {
